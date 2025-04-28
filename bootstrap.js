6f4249b08649ab68636245a77e07bc75ad5bcee1
Øyvind Solheim <oyvindbs@gmail.com>
2025-04-28 07:32:34 +0000

    new bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -1,34 +1,55 @@
-var bootstrap = {
-    startup: function(data, reason) {
-      // Load the main script when Zotero is ready
-      Components.utils.import("resource://gre/modules/Services.jsm");
-      
-      // Wait for Zotero to be fully loaded
-      Services.obs.addObserver(function observer(subject, topic, data) {
-        Services.obs.removeObserver(observer, topic);
-        
-        // Load the main script
+function startup(data, reason) {
+    // Import required modules
+    Components.utils.import("resource://gre/modules/Services.jsm");
+    
+    // Log startup
+    Services.console.logStringMessage("ZotShelf: Plugin starting up!");
+    
+    // Wait for Zotero to be fully loaded before initializing our plugin
+    if (typeof Zotero === 'undefined') {
+        Services.obs.addObserver(function observer(subject, topic, data) {
+            Services.obs.removeObserver(observer, topic);
+            initializeZotShelf();
+        }, "zotero-loaded", false);
+    } else {
+        initializeZotShelf();
+    }
+}
+
+function initializeZotShelf() {
+    Services.console.logStringMessage("ZotShelf: Initializing plugin...");
+    
+    try {
+        // Try to load our main plugin file
         let loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
                     .getService(Components.interfaces.mozIJSSubScriptLoader);
-        
-        // Load our main script
+                    
+        // Load the main script - this will execute ZotShelf.init()
         loader.loadSubScript("chrome://zotshelf/content/zotshelf.js");
-      }, "zotero-loaded", false);
-    },
+        
+        Services.console.logStringMessage("ZotShelf: Main script loaded successfully");
+    } catch (e) {
+        Services.console.logStringMessage("ZotShelf: Error loading main script: " + e);
+    }
+}
+
+function shutdown(data, reason) {
+    Services.console.logStringMessage("ZotShelf: Plugin shutting down");
     
-    shutdown: function(data, reason) {
-      // Call the shutdown function if it exists
-      if (typeof onShutdown === 'function') {
-        onShutdown();
-      }
-    },
-    
-    install: function(data, reason) {},
-    uninstall: function(data, reason) {}
-  };
-  
-  // Export the bootstrap methods
-  function startup(data, reason) { bootstrap.startup(data, reason); }
-  function shutdown(data, reason) { bootstrap.shutdown(data, reason); }
-  function install(data, reason) { bootstrap.install(data, reason); }
-  function uninstall(data, reason) { bootstrap.uninstall(data, reason); }
+    // Try to run the onShutdown function if it exists
+    if (typeof onShutdown === 'function') {
+        try {
+            onShutdown();
+        } catch (e) {
+            Services.console.logStringMessage("ZotShelf: Error during shutdown: " + e);
+        }
+    }
+}
+
+function install(data, reason) {
+    Services.console.logStringMessage("ZotShelf: Plugin installed");
+}
+
+function uninstall(data, reason) {
+    Services.console.logStringMessage("ZotShelf: Plugin uninstalled");
+}
